35774f2eef39811059ba199399dee8edd36d5cc5
NSgopi2001 <gopishankarns2001@gmail.com>
2024-11-27 15:32:04 +0530

    PlayerController
    Added basic movement and player

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -9,11 +9,16 @@ public class BoardManager : MonoBehaviour
     public int Height;
     public Tile[] GroundTiles;
     public Tile[] WallTiles;
+    private Grid m_Grid;
+
+    public PlayerController Player;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
+
+        m_Grid = GetComponentInChildren<Grid>();
 
         m_BoardData = new CellData[Width, Height];
 
@@ -38,7 +43,25 @@ public class BoardManager : MonoBehaviour
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
+            
         }
+        Player.Spawn(this, new Vector2Int(1, 1));
+    }
+
+    public Vector3 CellToWorld(Vector2Int cellIndex)
+    {
+        return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
+    }
+
+    public CellData GetCellData(Vector2Int cellIndex)
+    {
+        if (cellIndex.x < 0 || cellIndex.x >= Width
+            || cellIndex.y < 0 || cellIndex.y >= Height)
+        {
+            return null;
+        }
+
+        return m_BoardData[cellIndex.x, cellIndex.y];
     }
 
     public class CellData
