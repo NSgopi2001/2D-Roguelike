a69ff6b3fe1fb80c76aa0d3f318f5879d0f7f9e1
NSgopi2001 <gopishankarns2001@gmail.com>
2024-11-27 14:00:44 +0530

    Cell data
    Added cell data class to board manager

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -15,19 +15,25 @@ public class BoardManager : MonoBehaviour
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
 
+        m_BoardData = new CellData[Width, Height];
+
+
         for (int y = 0; y < Height; ++y)
         {
             for (int x = 0; x < Width; ++x)
             {
                 Tile tile;
+                m_BoardData[x, y] = new CellData();
 
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
                     tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
                     tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    m_BoardData[x,y].Passable = true;
                 }
 
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
@@ -35,4 +41,9 @@ public class BoardManager : MonoBehaviour
         }
     }
 
+    public class CellData
+    {
+        public bool Passable;
+    }
+    private CellData[,] m_BoardData;
 }
