7e3ba559a52c72f0225d8d435b517011c889120d
NSgopi2001 <gopishankarns2001@gmail.com>
2024-12-04 15:28:36 +0530

    Smooth movement
    Added move towards function for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,8 +8,14 @@ public class PlayerController : MonoBehaviour
 
     private bool m_IsGameOver;
 
+    public float MoveSpeed = 5.0f;
+
+    private bool m_IsMoving;
+    private Vector3 m_MoveTarget;
+
     public void Init()
     {
+        m_IsMoving = false;
         m_IsGameOver = false;
     }
 
@@ -21,13 +27,23 @@ public class PlayerController : MonoBehaviour
     public void Spawn(BoardManager boardManager, Vector2Int cell)
     {
         m_Board = boardManager;
-        MoveTo(cell);
+        MoveTo(cell,true);
     }
 
-    public void MoveTo(Vector2Int cell)
+    public void MoveTo(Vector2Int cell, bool immediate)
     {
         m_CellPosition = cell;
-        transform.position = m_Board.CellToWorld(m_CellPosition);
+
+        if (immediate)
+        {
+            m_IsMoving = false;
+            transform.position = m_Board.CellToWorld(m_CellPosition);
+        }
+        else
+        {
+            m_IsMoving = true;
+            m_MoveTarget = m_Board.CellToWorld(m_CellPosition);
+        }
     }
 
 
@@ -42,6 +58,22 @@ public class PlayerController : MonoBehaviour
 
             return;
         }
+
+        if (m_IsMoving)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_MoveTarget, MoveSpeed * Time.deltaTime);
+
+            if (transform.position == m_MoveTarget)
+            {
+                m_IsMoving = false;
+                var cellData = m_Board.GetCellData(m_CellPosition);
+                if (cellData.ContainedObject != null)
+                    cellData.ContainedObject.PlayerEntered();
+            }
+
+            return;
+        }
+
 
         Vector2Int newCellTarget = m_CellPosition;
         bool hasMoved = false;
@@ -67,26 +99,25 @@ public class PlayerController : MonoBehaviour
             hasMoved = true;
         }
 
-        if (hasMoved)
+        if(hasMoved)
         {
-            //check if the new position is passable, then move there if it is.
-            BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
+           //check if the new position is passable, then move there if it is.
+           BoardManager.CellData cellData = m_Board.GetCellData(newCellTarget);
 
-            if (cellData != null && cellData.Passable)
-            {
-                GameManager.Instance.TurnManager.Tick();
-                
-                if (cellData.ContainedObject == null)
-                {
-                    MoveTo(newCellTarget);
-                }
-                else if (cellData.ContainedObject.PlayerWantsToEnter())
-                {
-                    MoveTo(newCellTarget);
-                    //Call PlayerEntered AFTER moving the player! Otherwise not in cell yet
-                    cellData.ContainedObject.PlayerEntered();
-                }
-            }
+           if(cellData != null && cellData.Passable)
+           {
+               GameManager.Instance.TurnManager.Tick();
+
+               if (cellData.ContainedObject == null)
+               {
+                   MoveTo(newCellTarget, false);
+               }
+               else if(cellData.ContainedObject.PlayerWantsToEnter())
+               {
+                   MoveTo(newCellTarget, false);
+               }
+               
+           }
         }
 
     }
